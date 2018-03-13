73a3d636061ccfe68b0bd981b567eff2e33b4157
Sander van Dragt <sander@vandragt.com>
2018-03-13 21:41:39 +0000

    stubbing

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def show_index():
     return 'Hello World!'
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+@app.route('/user/')
+@app.route('/user/<username>')
+def show_user_profile(username=None):
+    return render_template('user_profile.html', username=username)
+
+
+def do_the_login():
+    pass
+
+def show_the_login_form():
+    pass
 
 if __name__ == '__main__':
     app.run()
