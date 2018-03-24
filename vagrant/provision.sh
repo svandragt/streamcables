9ec97ec84973a57e2127ecbdb8b5d0f6f84866c3
Sander van Dragt <sander@vandragt.com>
2018-03-24 15:12:30 +0000

    Python 3.5 to 3.6

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update
 apt-get clean
 apt-get -y autoremove
 
-# Install opam
-apt-get -y install m4 unzip build-essential aspcud mercurial git darcs
+apt-get -y install python3.6 m4 unzip build-essential aspcud mercurial git darcs
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
