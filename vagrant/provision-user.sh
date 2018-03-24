90bef9bfb951bcada5af4c5dd3f2c29f8005be8d
Sander van Dragt <sander@vandragt.com>
2018-03-24 12:45:39 +0000

    Install liquidsoap in a vagrant environment

---
+++ vagrant/provision-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+# Install liquidsoap
+/usr/local/bin/opam init --comp 4.05.0 -a
+eval `opam config env`
+opam install depext -y
+opam depext taglib mad lame vorbis cry samplerate liquidsoap -y
+opam install taglib mad lame vorbis cry samplerate liquidsoap -y
---
+++ vagrant/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get clean
+apt-get -y autoremove
+
+# Install opam
+apt-get -y install m4 unzip build-essential aspcud mercurial git darcs
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+
