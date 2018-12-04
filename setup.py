d8e5108697667182e48c1bbaf293a93490d49381
Sander van Dragt <sander@vandragt.com>
2018-12-04 20:31:10 +0000

    packaging story

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='StreamCables',
+    version='0.1',
+    packages=['streamcables',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
